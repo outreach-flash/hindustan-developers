ea615a31aec789d1652ee3531703b2a834c5ceb2
Bhadraksh Bhargava <outreach@sol4you.in>
2026-09-10 14:34:33 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [keystatic()],
   vite: {
     plugins: [tailwindcss()],
